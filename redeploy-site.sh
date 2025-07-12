4e202d7216212675c4f3af14d31748037e0aeb56
AliMoamen <alimoamen100@gmail.com>
2025-07-12 15:58:59 +0300

    Fix null directory in redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd aosman25-portfolio-mlh && \
-git fetch && git reset origin/main --hard > /dev/null/ && \
+git fetch && git reset origin/main --hard > /dev/null && \
 if [ ! -d python3-virtualvenv ]; then python3 -m venv python3-virtualvenv; fi && \
-source ./python3-virtualvenv/bin/activate > /dev/null/ && \
-pip install -r requirements.txt > /dev/null/ && \
+source ./python3-virtualvenv/bin/activate > /dev/null && \
+pip install -r requirements.txt > /dev/null && \
 systemctl restart myportfolio &&\
 echo "Successfully Restarted myportfolio Serivce!"

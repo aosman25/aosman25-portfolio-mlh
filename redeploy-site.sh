77cf9300816a3e80eee01b204d83db4f90173a5b
AliMoamen <alimoamen100@gmail.com>
2025-07-12 15:55:56 +0300

    Update redeploy-site.sh to adapt with the new service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Kill old tmux session if exists
-tmux kill-session -t portfolio-session 2>/dev/null
-
-# Start new detached tmux session and run everything inside it
-tmux new-session -d -s portfolio-session "
 cd aosman25-portfolio-mlh && \
-git fetch && git reset origin/main --hard && \
+git fetch && git reset origin/main --hard > /dev/null/ && \
 if [ ! -d python3-virtualvenv ]; then python3 -m venv python3-virtualvenv; fi && \
-source ./python3-virtualvenv/bin/activate && \
-pip install -r requirements.txt && \
-flask run --host 0.0.0.0
-"
+source ./python3-virtualvenv/bin/activate > /dev/null/ && \
+pip install -r requirements.txt > /dev/null/ && \
+systemctl restart myportfolio &&\
+echo "Successfully Restarted myportfolio Serivce!"

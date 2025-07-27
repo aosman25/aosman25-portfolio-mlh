4d3b332e59f071be1b3a79c84fc4cd424557b339
AliMoamen <alimoamen100@gmail.com>
2025-07-26 19:15:24 -0700

    Update redeploy bash scrip to run with docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 cd aosman25-portfolio-mlh && \
 git fetch && git reset origin/main --hard > /dev/null && \
-if [ ! -d python3-virtualenv ]; then python3 -m venv python3-virtualenv; fi && \
-source ./python3-virtualenv/bin/activate > /dev/null && \
-pip install -r requirements.txt > /dev/null && \
-systemctl restart myportfolio &&\
-echo "Successfully Restarted myportfolio Serivce!"
+docker compose -f docker-compose.prod.yml down && \
+docker compose -f docker-compose.prod.yml up -d && \

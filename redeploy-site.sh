8ae39341603335bb1f0d20fb3cd3029a0b2a7a37
AliMoamen <alimoamen100@gmail.com>
2025-07-26 19:33:07 -0700

    Fix syntax error in the redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,4 +2,4 @@
 cd aosman25-portfolio-mlh && \
 git fetch && git reset origin/main --hard > /dev/null && \
 docker compose -f docker-compose.prod.yml down && \
-docker compose -f docker-compose.prod.yml up -d && \
+docker compose -f docker-compose.prod.yml up -d
